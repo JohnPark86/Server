a3ff7e1d0bd58029b7f63934ea3b40e99a1116ff
John <jduncanpark@gmail.com>
2016-11-20 00:43:22 -0500

    Update file with each page refresh

--- src/com/java/serverTest/FileCounter.java
+++ src/com/java/serverTest/FileCounter.java
@@ -2,19 +2,25 @@ package com.java.serverTest;
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class FileCounter
  */
 @WebServlet("/FileCounter")
 public class FileCounter extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-       
+    int count;
+    private FileDao dao;
+    
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +28,35 @@ public class FileCounter extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    @Override
+    public void init() throws ServletException {
+            dao = new FileDao();
+            try {
+                    count = dao.getCount();
+            } catch (Exception e) {
+                    getServletContext().log("An exception occurred in FileCounter", e);
+                    throw new ServletException("An exception occurred in FileCounter"
+                                    + e.getMessage());
+            }
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		HttpSession session = request.getSession(true);
+		
+		session.setMaxInactiveInterval(5);
+		response.setContentType("text/plain");
+		
+		PrintWriter out = response.getWriter();
+		if(session.isNew()){
+			count++;
+		}
+		
+		out.println("This site has been accessed " + count + " times");
 	}
 
 	/**
@@ -37,4 +66,12 @@ public class FileCounter extends HttpServlet {
 		// TODO Auto-generated method stub
 	}
 
+	public void destroy() {
+        super.destroy();
+        try {
+                dao.save(count);
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+}
 }
